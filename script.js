2635a8c3172c2b26de1b1202278c8298eea6fdd5
unknown <>
2016-03-03 12:51:29 +0900

    もっと詳しく完成

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ $(function() {
 
 //もっと詳しくボタンNG
 
-$('.btn-top-white').click(function(){
-  
-  if(('#read-more').hasClass('open')){
-   $('#detail').fadeOut();
-   $('#read-more').removeClass('open').text("もっと詳しく");
+  $('.btn-top-white').click(function() {
+    if($('#read-more').hasClass('open')) {
+     $('#detail').fadeOut();
+      $('#read-more').removeClass('open').text("もっと詳しく");
 
-} else {
-	$('#read-more').addClass('open').text("閉じる");
-	$('#detail').fadeIn();
+    } else {
+     $('#read-more').addClass('open').text("閉じる");
+       $('#detail').fadeIn();
 	
-}
-});
+    }
+  });
 
 //学習する言語OK
 	
-$('.language-box').hover(function() {
-      $(this).find('.text-active').fadeIn(500);
-    },
-    function() {
-      $(this).find('.text-active').fadeOut(500);
-    }
-  );
+  $('.language-box').hover(function() {
+   $(this).find('.text-active').fadeIn(500);
+  },
+   function() {
+    $(this).find('.text-active').fadeOut(500);
+  });
+  
 
 
 
-});
+
+  }); //TOPのfanction
