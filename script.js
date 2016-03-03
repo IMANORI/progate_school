e312148fef22b1905b1d3254d4c55fcb5745feb4
unknown <>
2016-03-03 16:11:23 +0900

    スライドOK

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 $(function() {
  
-// スクロール難しい
+	// スクロール難しい
 
 
 
 
-//もっと詳しくボタンNG
+	//もっと詳しくボタンOK
 
-  $('.btn-top-white').click(function() {
-    if($('#read-more').hasClass('open')) {
-     $('#detail').fadeOut();
-      $('#read-more').removeClass('open').text("もっと詳しく");
+	  $('.btn-top-white').click(function() {
+	    if($('#read-more').hasClass('open')) {
+	     $('#detail').fadeOut();
+	      $('#read-more').removeClass('open').text("もっと詳しく");
 
-    } else {
-     $('#read-more').addClass('open').text("閉じる");
-       $('#detail').fadeIn();
-	
-    }
-  });
+	    } else {
+	     $('#read-more').addClass('open').text("閉じる");
+	       $('#detail').fadeIn();
+		
+	    }
+	  });
 
-//学習する言語OK
-	
-  $('.language-box').hover(function() {
-   $(this).find('.text-active').fadeIn(500);
-  },
-   function() {
-    $(this).find('.text-active').fadeOut(500);
-  });
+	//学習する言語OK
+		
+	  $('.language-box').hover(
+	  function() {
+	   $(this).find('.text-active').fadeIn(500);
+	  },
+	   
+	   function() {
+	    $(this).find('.text-active').fadeOut(500);
+	   });
   
+    
+    //画像ZOOM
+
+    　　$('.column-box').hover(
+       function() {
+        $(this).find('.column-box img').css('width','300px').css('overflow','hidden');
+       },
+        
+        function() {
+         $(this).find('.column-box img').css('width','100%');
+        });
+
+    //スライダー OK
+
+    　　　$(document).ready(function(){
+  　　　　　　$('.bxslider').bxSlider();
+        });
 
 
-
-
-  }); //TOPのfanction
+}); //TOPのfanction
